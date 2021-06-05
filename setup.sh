d320b9c64d5d08edad3b98bedd0ce339179507bf
Nasium Hill <zainullinaaliya45@gmail.com>
2021-06-05 16:35:39 +0300

    checkers added

--- setup.sh
+++ setup.sh
@@ -17,29 +17,29 @@ docker build -t image_nginx ./srcs/nginx
 kubectl apply -f srcs/nginx/nginx.yaml
 
 #mysql
-#docker build -t image_mysql ./srcs/mysql/
-#kubectl apply -f srcs/mysql/mysql.yaml
+docker build -t image_mysql ./srcs/mysql/
+kubectl apply -f srcs/mysql/mysql.yaml
 
 #wordpress
 docker build -t image_wordpress ./srcs/wordpress/
 kubectl apply -f srcs/wordpress/wordpress.yaml
 
 #phpmyadmin
-#docker build -t image_phpmyadmin ./srcs/phpmyadmin/
-#kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
+docker build -t image_phpmyadmin ./srcs/phpmyadmin/
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 
 
 #influx
-#docker build -t image_influxdb ./srcs/influxdb/
-#kubectl apply -f srcs/influxdb/influxdb.yaml
+docker build -t image_influxdb ./srcs/influxdb/
+kubectl apply -f srcs/influxdb/influxdb.yaml
 
 
 #ftps
-#docker build -t image_ftps ./srcs/ftps/
-#kubectl apply -f srcs/ftps/ftps.yaml
+docker build -t image_ftps ./srcs/ftps/
+kubectl apply -f srcs/ftps/ftps.yaml
 
 #grafana
-#docker build -t image_grafana ./srcs/grafana/
-#kubectl apply -f srcs/grafana/grafana.yaml
+docker build -t image_grafana ./srcs/grafana/
+kubectl apply -f srcs/grafana/grafana.yaml
 
 minikube dashboard
